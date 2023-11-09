667d9b0198de804ec0641d2a5d2a8fcfb41883a0
Giuseppe Minnella <giuseppe.minnella@gmail.com>
2023-11-09 09:39:15 +0100

    Replace request-promise with axios, add base url as parameter, list contributors

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const rp = require('request-promise'),
-    crypto = require('crypto');
+const axios = require('axios');
+const crypto = require('crypto');
 
-module.exports = class API {
-    constructor(user, pass) {
+module.exports = class EmarsysAPI {
+    constructor(user, pass, baseUrl = 'https://api.emarsys.net/api/v2') {
         this.user = user;
         this.pass = pass;
-        this.url = 'https://api.emarsys.net/api/v2';
+        this.url = baseUrl;
     }
 
     base64Sha1(str) {
@@ -17,9 +17,9 @@ module.exports = class API {
     }
 
     getWsseHeader(user, pass) {
-        let nonce = crypto.randomBytes(16).toString('hex');
-        let timestamp = new Date().toISOString();
-        let digest = this.base64Sha1(nonce + timestamp + pass);
+        const nonce = crypto.randomBytes(16).toString('hex');
+        const timestamp = new Date().toISOString();
+        const digest = this.base64Sha1(nonce + timestamp + pass);
         return 'UsernameToken Username="' + user + '", PasswordDigest="' + digest + '", Nonce="' + nonce + '", Created="' + timestamp + '"';
     }
 
@@ -47,7 +47,8 @@ module.exports = class API {
     async get(uri, data) {
         try {
             const options = this.setOptions(uri, {qs: data});
-            return rp.get(options);
+            const response = await axios.get(options.url, options);
+            return response.data;
         } catch (error) {
             error.message = `Error making GET request to ${uri}: ${error.message}`;
             throw error;
@@ -57,7 +58,8 @@ module.exports = class API {
     async put(uri, data) {
         try {
             const options = this.setOptions(uri, {body: JSON.stringify(data)});
-            return rp.put(options);
+            const response = await axios.put(options.url, options.body, options);
+            return response.data;
         } catch (error) {
             error.message = `Error making PUT request to ${uri}: ${error.message}`;
             throw error;
@@ -67,7 +69,8 @@ module.exports = class API {
     async post(uri, data) {
         try {
             const options = this.setOptions(uri, {body: JSON.stringify(data)});
-            return rp.post(options);
+            const response = await axios.post(options.url, options.body, options);
+            return response.data;
         } catch (error) {
             error.message = `Error making POST request to ${uri}: ${error.message}`;
             throw error;
@@ -77,7 +80,8 @@ module.exports = class API {
     async delete(uri, data) {
         try {
             const options = this.setOptions(uri, {qs: data});
-            return rp.delete(options);
+            const response = await axios.delete(options.url, options);
+            return response.data;
         } catch (error) {
             error.message = `Error making DELETE request to ${uri}: ${error.message}`;
             throw error;
